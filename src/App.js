a00954cd0b64d5e525abccee2af438677414bbd8
Ragavan-M7 <ragava15052004@gmail.com>
2025-03-05 11:40:12 +0530

    commit the project

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return <>
   <h1>This is React Project</h1>
   <h2>Some Changes</h2>
+  <h2 style={{color:'red', textAlign:"center", fontSize:'30px'}}>redeploy the project</h2>
   </>
 }
 
