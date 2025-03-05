3c01f02b2f17791e2941432ba9ccf0bda2ba11ef
Ragavan-M7 <ragava15052004@gmail.com>
2025-03-05 11:24:30 +0530

    new project

---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav>
+        <li>home</li>
+        <li>about</li>
+        <li>contact</li>
+    </nav>
+  )
+}
+
+export default Navbar
